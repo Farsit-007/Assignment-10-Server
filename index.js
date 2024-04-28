92a9405cca5ba462dc5133bfc097b07d32659151
Farsit-007 <public>
2024-04-29 01:21:36 +0600

    2nd Collection Data Added

--- index.js
+++ index.js
@@ -96,7 +96,13 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/category', async (req, res) => {
+            const cursor = SubCategoryCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
         
+
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
